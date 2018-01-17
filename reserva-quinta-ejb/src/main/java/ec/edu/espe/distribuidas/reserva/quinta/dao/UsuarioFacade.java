6efa7f7bb0bdb23d52e89e8aff65acabba2ba58b
Eliana Vasquez <elvasques@espe.edu.ec>
2018-01-16 23:50:32 -0500

    Login y Plantillas

---
+++ reserva-quinta-ejb/src/main/java/ec/edu/espe/distribuidas/reserva/quinta/dao/UsuarioFacade.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.distribuidas.reserva.quinta.dao;
+
+import ec.edu.espe.distribuidas.reserva.quinta.model.Usuario;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Ing Alvaro Vasquez
+ */
+@Stateless
+@LocalBean
+public class UsuarioFacade extends AbstractFacade<Usuario> {
+
+    @PersistenceContext(unitName = "ec.edu.espe.distribuidas_reserva-quinta-ejb_ejb_1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    public UsuarioFacade(){
+        super(Usuario.class);
+    }
+}
